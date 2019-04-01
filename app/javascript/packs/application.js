d0b8006b5eb87664220642bd42074b2733336e68
Nuno Loureiro <nunoloureiro88@gmail.com>
2019-04-01 09:48:26 +0100

    Removed basic tootip JS function to prevent tips on mobile

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,17 +3,17 @@ import "bootstrap";
 //= require data-confirm-modal
 
 
-$(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip(
-      {container:'body', trigger: 'hover', placement:"top"}
-    );
-  });
+// $(document).ready(function(){
+//     $('[data-toggle="tooltip"]').tooltip(
+//       {container:'body', trigger: 'hover', placement:"top"}
+//     );
+//   });
 
-$(document).ready(function(){
-    $('[data-toggle="tooltip-bottom"]').tooltip(
-      {container:'body', trigger: 'hover', placement:"bottom"}
-    );
-  });
+// $(document).ready(function(){
+//     $('[data-toggle="tooltip-bottom"]').tooltip(
+//       {container:'body', trigger: 'hover', placement:"bottom"}
+//     );
+//   });
 
 function isTouchDevice(){
     return true == ("ontouchstart" in window || window.DocumentTouch && document instanceof DocumentTouch);
